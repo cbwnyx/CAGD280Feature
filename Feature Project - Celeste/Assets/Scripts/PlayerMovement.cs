222152021a9093f16d0d8684995dfed3ee3b1ba5
cbwnyx <mominepoomp@gmail.com>
2023-12-02 12:13:18 -0800

    added wall slide
    and dash

--- Feature Project - Celeste/Assets/Scripts/PlayerMovement.cs
+++ Feature Project - Celeste/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,7 @@ using UnityEngine.InputSystem;
 //This script is in charge of the player movement.
 public class PlayerMovement : MonoBehaviour
 {
-    //private PlayerInputActions playerActions;
-    //private Vector2 moveVec = Vector2.zero;
-    
+
     private Rigidbody rb;
 
     public float speed = 4f;
@@ -19,20 +17,33 @@ public class PlayerMovement : MonoBehaviour
     public bool isGrounded = true;
     public float jumpForce = 10f;
     public float gravityScale = 2f;
-    //public float jumpHeight = 10f;
-    
-    //public float startTime;
-    //public float timeSpacePressed;
+
+    private bool wallSliding = false;
+    private float wallSlideSpeed = 2f;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 30f;
+    private float dashTime = 0.2f;
+    private Vector2 dashDir;
+    private float dashCooldown = 1f;
+
+    public bool touchingWall;
+
+    //Serialize fields here to be able to reference the wallcheck
+    [SerializeField] private Transform wallCheck;
+    //[SerializeField] private LayerMask wallLayer;
+    public LayerMask wallLayer;
 
     private void Awake()
     {
-        //playerActions = new PlayerInputActions();
         rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         moveDir = Input.GetAxisRaw("Horizontal");
+
     }
 
     private void FixedUpdate()
@@ -40,11 +51,15 @@ public class PlayerMovement : MonoBehaviour
         jump();
         run();
         increaseGravity();
+        //wallSlide();
+        wallTouch();
+        playerDash();
     }
 
     private void run()
     {
         //causes player to accelerate when initially moving instead of immediately moving at max speed
+        
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
             if (speed < 6.5f)
@@ -61,8 +76,20 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        if (touchingWall == false)
+        {
+            rb.velocity = new Vector2(moveDir * speed, rb.velocity.y);
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                touchingWall = false;
+                rb.velocity = new Vector2(moveDir * speed, rb.velocity.y);
+            }
+        }
         ////rb.velocity.y is stated as such because we are not messing with the y value, so it should stay the same
-        rb.velocity = new Vector2(moveDir * speed, rb.velocity.y);
+        
 
     }
 
@@ -119,87 +146,79 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    private void wallGrab()
+    /*private bool touchingWall()
     {
-        if (Input.GetKey("z"))
+        //this tells us whether the empty game object "WallCheck" is making contact with a wall
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+        Debug.Log("Touched Wall");
+    }*/
+
+    private void wallTouch()
+    {
+        RaycastHit wallHit;
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out wallHit, Mathf.Infinity);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * wallHit.distance, Color.red);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out wallHit, 1f))
         {
-            rb.velocity = new Vector2(0,0);
-            Debug.Log("Grabbed Wall");
+            if (isGrounded == false && moveDir != 0f)
+            {
+                wallSliding = true;
+                // Mathf.Clamp keeps the values of the velocity between the given range
+                rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+            }
         }
+    }
+
+    /*private void wallSlide()
+    {
+        //if the player is touching the wall and is not grounded and there is a directional input, the player will wall slide
+        if (touchingWall() && isGrounded == false && moveDir != 0f)
+        {
+            wallSliding = true;
+            // Mathf.Clamp keeps the values of the velocity between the given range
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            wallSliding = false;
+        }
+    }*/
+
+    private void playerDash()
+    {
+        if (Input.GetKey("x") && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = gravityScale;
+        gravityScale = 0f;
+
+        dashDir = new Vector2(moveDir, Input.GetAxisRaw("Vertical"));
+        if (dashDir == Vector2.zero)
+        {
+            dashDir = new Vector2(transform.localScale.x, 0);
+        }
+        rb.velocity = dashDir.normalized * dashingPower;
+        yield return new WaitForSeconds(dashTime);
+        gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Wall")
         {
-            wallGrab();
+            touchingWall = true;
         }
-        
-
-
-        /*if (collision.gameObject.tag == "Wall")
-        {
-            Debug.Log("Touched Wall");
-            if (Input.GetKey("z"))
-            {
-                rb.velocity = new Vector2(0, 0);
-                Debug.Log("Pressed Grab");
-            }
-            else
-            {
-                rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
-            }
-        }*/
     }
-    /*private void OnEnable()
-    {
-        playerActions.Enable();
-        playerActions.PlayerActions.Move.performed += OnMove;
-        playerActions.PlayerActions.Move.canceled += OnMoveStop;
-
-    }
-    private void OnDisable()
-    {
-        playerActions.Disable();
-        playerActions.PlayerActions.Move.performed -= OnMove;
-        playerActions.PlayerActions.Move.canceled -= OnMoveStop;
-    }
-
-    private void OnMove(InputAction.CallbackContext value)
-    {
-        moveVec = value.ReadValue<Vector2>();
-        playerMoving = true;
-    }
-
-    //monitors the player movement when input is stopped
-    private void OnMoveStop(InputAction.CallbackContext value)
-    {
-        moveVec = Vector3.zero;
-        playerMoving = false;
-    }
-
-    //if speed is less than 6.5, accelerate
-    private void runAcceleration()
-    {
-        if (playerMoving == true)
-        {
-            if (speed < 6.5f)
-            {
-                speed += 15 * Time.deltaTime;
-            }
-        }
-        if (playerMoving == false)
-        {
-            if (speed > 4f)
-            {
-                speed -= 30 * Time.deltaTime;
-            }
-        }
-        //rb.velocity = moveVec * speed;
-
-    }*/
-
-
     //This is the logic i need
     //if the player inputs left or right, they will move in that direction. Done
     //if the player inputs left or right, they will accelerate from immobile to a max speed of, say, 8. Done (max speed 6.5)
